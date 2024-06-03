f9eb2b7e3b0a3567ebc446037faf8b99479bf135
krs9891 <krzysztof.russek@protonmail.com>
2024-06-03 15:53:11 +0200

    Handle exceptions when processing PDF files

--- clean.py
+++ clean.py
@@ -3,21 +3,29 @@ import argparse
 import PyPDF2
 import shutil
 
+import PyPDF2.errors
+
 def extract_pages_to_new_pdf(input_pdf_path):
     temp_output_pdf_path = 'temp_output.pdf'
 
-    with open(input_pdf_path, 'rb') as input_pdf_file:
-        reader = PyPDF2.PdfReader(input_pdf_file)
-        writer = PyPDF2.PdfWriter()
+    try:
+        with open(input_pdf_path, 'rb') as input_pdf_file:
+            reader = PyPDF2.PdfReader(input_pdf_file)
+            writer = PyPDF2.PdfWriter()
 
-        for page_num in range(len(reader.pages)):
-            writer.add_page(reader.pages[page_num])
+            for page_num in range(len(reader.pages)):
+                writer.add_page(reader.pages[page_num])
 
-        with open(temp_output_pdf_path, 'wb') as output_pdf_file:
-            writer.write(output_pdf_file)
+            with open(temp_output_pdf_path, 'wb') as output_pdf_file:
+                writer.write(output_pdf_file)
 
-    shutil.move(temp_output_pdf_path, input_pdf_path)
-    print(f"Cleaned {input_pdf_path}")
+        shutil.move(temp_output_pdf_path, input_pdf_path)
+        print(f"Cleaned {input_pdf_path}")
+    except PermissionError:
+        print(f"Could not clean {input_pdf_path} because it is currently in use.")
+        os.remove(temp_output_pdf_path)
+    except (PyPDF2.errors.EmptyFileError, PyPDF2.errors.PdfReadError):
+        print(f"Could not clean {input_pdf_path} because the file is damaged or unreadable.")
 
 def main():
     parser = argparse.ArgumentParser(description='Process some PDFs.')
