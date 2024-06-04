3d30c4e7cb0b6af697020b9a2c296a1eeac44952
krs9891 <krzysztof.russek@protonmail.com>
2024-06-04 16:54:09 +0200

    interface done

--- pdf_unlocker.py
+++ pdf_unlocker.py
@@ -1,9 +1,12 @@
 import os
-import argparse
 import PyPDF2
 import shutil
-
 import PyPDF2.errors
+
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+
+TEST_DIR = 'test-dir'
 
 def extract_pages_to_new_pdf(input_pdf_path):
     temp_output_pdf_path = 'temp_output.pdf'
@@ -27,18 +30,43 @@ def extract_pages_to_new_pdf(input_pdf_path):
     except (PyPDF2.errors.EmptyFileError, PyPDF2.errors.PdfReadError):
         print(f"Could not clean {input_pdf_path} because the file is damaged or unreadable.")
 
+def get_pdf_choices_from_dir():
+    files = [Choice(os.path.join(TEST_DIR, file)) for file in os.listdir(TEST_DIR) if file.endswith(".pdf")]
+    return files
+
 def main():
-    parser = argparse.ArgumentParser(description='Process some PDFs.')
-    parser.add_argument('files', metavar='F', type=str, nargs='*', help='a list of PDF files to process')
-    args = parser.parse_args()
+    files_list = get_pdf_choices_from_dir()
 
-    # hard-coded directory for development and testing
-    test_dir = 'test-dir'
+    action = inquirer.select(
+        message="What do you want to do?",
+        choices=[Choice(value="All", name="Unlock All PDFs"), 
+                 Choice(value="Select", name="Select PDFs")],
+        default="All"
+    ).execute()
 
-    files_to_process = args.files if args.files else [os.path.join(test_dir, file) for file in os.listdir(test_dir) if file.endswith(".pdf")]
+    if action == "All":
+        files_to_process = [file.value for file in files_list]
+    else:
+        files_to_process = inquirer.checkbox(
+            message="Select file(s):",
+            choices=files_list,
+            transformer=lambda result: "%s file%s selected"
+            % (len(result), "s" if len(result) > 1 else ""),
+            validate=lambda result: len(result) >= 1,
+            invalid_message="You must select at least one file."
+        ).execute()
 
-    for file in files_to_process:
-        extract_pages_to_new_pdf(file)
+        for file in files_to_process:
+            print(file)
+
+    proceed = inquirer.confirm(
+        message="Do you want to proceed?",
+        default=True
+    ).execute()
+
+    if proceed:
+        for file in files_to_process:
+            extract_pages_to_new_pdf(file)
 
 if __name__ == "__main__":
     main()
